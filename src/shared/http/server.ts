0255352ac5dbcb4099a5824dfbf7a2cfcc634fb2
As7Vezes <denysn60@gmail.com>
2022-09-08 12:37:51 -0300

    1

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -8,6 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'erro',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(2222, () => {
   console.log('server started on port 2222');
